6b8bcdcd56bd44278fb47bbc83974d1189b1910d
kevakba <akbarikevin@gmail.com>
2025-03-04 23:06:24 -0700

    KA - added partial forecasted data script

---
+++ scripts/forecasted_data_fetching/AIL.py
@@ -0,0 +1,43 @@
+import urllib.request
+import json
+import pandas as pd
+
+# Define start_date and end_date
+start_date = '2025-03-05'
+end_date = '2025-03-05'
+
+try:
+    # Construct the URL with the provided dates
+    url = f"https://apimgw.aeso.ca/public/actualforecast-api/v1/load/albertaInternalLoad?startDate={start_date}&endDate={end_date}"
+
+    hdr = {
+        # Request headers
+        'Cache-Control': 'no-cache',
+        'API-KEY': 'ff8bd8112f6942ac97bb08cc88d0deae',
+    }
+
+    req = urllib.request.Request(url, headers=hdr)
+
+    req.get_method = lambda: 'GET'
+    response = urllib.request.urlopen(req)
+    print(response.getcode())
+
+    response_data = response.read().decode('utf-8')
+
+    # Parse the response data as JSON
+    json_data = json.loads(response_data)
+    
+    df = pd.DataFrame(json_data)
+
+    # Initialize an empty DataFrame to store the output
+    out = pd.DataFrame()
+    
+    # Flatten the JSON data using the 'return' column
+    flattened_data = pd.json_normalize(df['return']['Actual Forecast Report'])
+    out['begin_datetime_mpt'] = flattened_data['begin_datetime_mpt'] 
+    out['forecast_alberta_internal_load'] = flattened_data['forecast_alberta_internal_load']
+
+    out.to_csv(f'/home/kevin/Downloads/BESS/data/raw/forecast/AIL_{start_date.replace("-", "")}_{end_date.replace("-", "")}.csv')
+
+except Exception as e:
+    print(e)
---
+++ scripts/forecasted_data_fetching/price.py
@@ -0,0 +1,50 @@
+import urllib.request
+import json
+from datetime import datetime
+import pandas as pd
+
+# Define start_date and end_date as datetime objects
+start_date = '2025-03-05'
+end_date = '2025-03-05'
+
+try:
+    # Construct the URL with the provided dates
+    url = f"https://apimgw.aeso.ca/public/poolprice-api/v1.1/price/poolPrice?startDate={start_date}&endDate={end_date}"
+
+    hdr = {
+        # Request headers
+        'Cache-Control': 'no-cache',
+        'API-KEY': 'ff8bd8112f6942ac97bb08cc88d0deae',
+    }
+
+    req = urllib.request.Request(url, headers=hdr)
+
+    req.get_method = lambda: 'GET'
+    response = urllib.request.urlopen(req)
+    print(response.getcode())
+
+    # Read and decode the response
+    response_data = response.read().decode('utf-8')
+    
+    # Parse the response data as JSON
+    json_data = json.loads(response_data)
+    
+    df = pd.DataFrame(json_data)
+
+    # Initialize an empty DataFrame to store the output
+    out = pd.DataFrame()
+
+    # Flatten the JSON data using the 'return' column
+    flattened_data = pd.json_normalize(df['return']['Pool Price Report'])
+
+    out['begin_datetime_mpt'] = flattened_data['begin_datetime_mpt']
+    out['forecast_pool_price'] = flattened_data['forecast_pool_price']
+    out['rolling_30day_avg'] = flattened_data['rolling_30day_avg']
+
+    out.to_csv(f'/home/kevin/Downloads/BESS/data/raw/forecast/price_{start_date.replace("-", "")}_{end_date.replace("-", "")}.csv')
+
+except Exception as e:
+    print(e)
+
+
+
